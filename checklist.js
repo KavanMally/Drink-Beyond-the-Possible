c4d9f909460ae836e284d7b018e9a45f4d55d163
Kavan Mally <kavan.mally@gmail.com>
2017-10-26 02:51:18 -0400

    checkbox test
    added test that checks that the label for the checkboxes match the value sent to the array from event handler.

--- checklist.js
+++ checklist.js
@@ -2,20 +2,15 @@
 var list = [];
 
 function addToArray(item){
-	
-	
 	list.push(item);
 	console.log(getGlobalArray());
 }
 
 function removeFromArray(item){
-
 	var index = list.indexOf(item);
 	list.splice(index, 1);
 	
 	console.log(getGlobalArray());
-
-	
 }
 
 function printArray(){ console.log(list); }
@@ -62,8 +57,6 @@ function createCheckList(){
 			checkbox.value = ingredient;
 			checkbox.id = ingredient;
 			
-			//console.log("sanity check: " + ingredient);
-			//checkbox.onclick = function(){checkboxHandler(checkbox.value);};
 			checkbox.addEventListener("change", function(){
 				if(this.checked){ addToArray(this.value); }
 				else{ removeFromArray(this.value);  }
@@ -77,67 +70,15 @@ function createCheckList(){
 			li.appendChild(checkbox);
 			li.appendChild(label);
 			
-			//document.body.appendChild(checkbox);
-			//document.body.appendChild(label);
-		
-			//document.body.appendChild(li);
-			
 			ul.appendChild(li);
 		
 		}
 		
 		div.appendChild(ul);
-		
-		
-		//document.body.appendChild(ul);
 		document.body.appendChild(div);
 
 		
-		
-		/*
-		var checkbox = document.createElement('input');
-		checkbox.type = "checkbox";
-		checkbox.name = "name";
-		checkbox.value = "value";
-		checkbox.id = "id";
-		
-		var label = document.createElement('label')
-		label.htmlFor = "id";
-		label.appendChild(document.createTextNode(data.drinks[0].strIngredient1));
-		
-		
-		document.body.appendChild(checkbox);
-		document.body.appendChild(label);
-		*/
 	});
 	
 }
 
-/*
-function createCookie(){
-	
-	var json_str = JSON.stringify(list);
-	//createCookie('mycookie', json_str);
-	
-	var days = 2;
-	
-	var d = new Date();
-	d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-	var expires = "expires=" + d.toUTCString();
-	document.cookie = "ingredients = " + json_str + ";" + expires + ";path=/";
-	
-	console.log("ingredients = " + json_str + ";" + expires + ";path=/");
-}
-
-function deleteCookie(){
-
-	document.cookie = "ingredients=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-	
-}
-
-function getCookie(){
-		
-	var cookie = document.cookie;
-	console.log(cookie);
-}
-*/
--- test.js
+++ test.js
@@ -5,3 +5,40 @@ function testGlobalArray(){
 	var x = getGlobalArray();
 	printArray();
 }
+
+
+function unit_testing(){
+	
+	
+}
+
+
+//go through all checkboxes and check that the label matches id and value
+function testCheckBoxes(){
+
+	var unorderedlist = document.getElementById("myul");
+	var listItem = unorderedlist.getElementsByTagName("li");
+	
+	for(var i = 0; i < listItem.length; i++){
+		
+		var x = listItem[i].childNodes;
+		if(x[0].value !== x[1].innerHTML) 
+			console.log("Error! Label not the same as value!");
+	}
+	
+	console.log("Checkboxes created successfully!");
+	
+}
+
+function testAdding(){
+	
+	
+}
+
+
+//check to make sure html code is complete (ie no missing tags)
+function testHTMLCode(){
+	
+	
+	
+}
