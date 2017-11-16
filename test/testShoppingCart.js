9b3dce47688a2e042bfb4e3ea16d10eac2073365
Vishal Patel <vsp20@case.edu>
2017-11-15 20:31:05 -0500

    unworking test
    this test doesnt work

--- test/testShoppingCart.js
+++ test/testShoppingCart.js
@@ -3,21 +3,21 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 var request = require('request');
 //var cart = require('../index.js');
-describe('http response code for cocktaildb', function(done){
-	it('should return 200', function(done){
-		setTimeout(function(){
-			try{
-				request.get('http://www.thecocktaildb.com/api/json/v1/1/random.php', function(err, res, body){
-				expect(res.statusCode).to.equal(200);
-				expect(res.body).to.equal('wrong header');
-				done();
-				});
-			}
-			catch(e){
-				done(e);
-			}
+// describe('http response code for cocktaildb', function(done){
+// 	it('should return 200', function(done){
+// 		setTimeout(function(){
+// 			try{
+// 				request.get('http://www.thecocktaildb.com/api/json/v1/1/random.php', function(err, res, body){
+// 				expect(res.statusCode).to.equal(200);
+// 				expect(res.body).to.equal('wrong header');
+// 				done();
+// 				});
+// 			}
+// 			catch(e){
+// 				done(e);
+// 			}
 			
 		
-		});
-	});
-});
+// 		});
+// 	});
+// });
