45c3d22bee86e926898196fd99222b3123298833
Vishal Patel <vsp20@case.edu>
2017-11-20 13:33:05 -0500

    Drink Search
    Added Drink search and template to the bootstrap template, results tab
    doesnt scroll to #template yet

--- main folder/js/index.js
+++ main folder/js/index.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	var data1="";
 	var instructions="";
 	var statCode;
+	var ingredients = " ";
 
 	var setValue = function(attribute, value){
 		$(attribute).html(value).fadeIn('3000');
@@ -37,7 +38,8 @@ $(document).ready(function() {
 			},
 			// Everything that happens after clicking the random option 
 			success: function(data){
-				$('#container').load('drinkTemplate1.html');
+				//$('#templateContainer').load('drinkTemplate1.html');
+				//loadTemplate();
 				name = data.drinks[0].strDrink;
 				img = data.drinks[0].strDrinkThumb;
 				// console.log(data);
@@ -63,7 +65,7 @@ $(document).ready(function() {
 			$("#drinkName").attr('font-size', "100px;");
 			setValue('#drinkName', name);
 			//$('img').attr('src',img);
-			attribute('img','src',img);
+			attribute('.templateImage','src',img);
 			//css('img','width','50px;');
 			//$('img').attr("width","200px");
 			css('img','height','100px;');
@@ -73,25 +75,110 @@ $(document).ready(function() {
 				str = data1.drinks[0]["strIngredient" + number];
 				amount = data1.drinks[0]["strMeasure" + number];
 				number = number + 1;
+				console.log(str);
 				console.log(amount);
 				$('#ingredients').append("<p>" + amount + " " + str + "</p>");
 			}
-			$('#instructions').append("<p>"  + instructions + "</p");
+			$('#instructions').append("<p> Instructions: "  + instructions + "</p");
 			//$('img').attr()
+		},2000);
+		
+	});
+
+	//Button for the find drink by manual ingredient search
+	$('.findDrinkButton').on('click', function(){
+		console.log("here");
+		var drinkValue = document.getElementById('manualSearch').value;
+		console.log(drinkValue);
+		$.ajax({
+			url:'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + drinkValue,
+			dataType: 'json',
+			type: 'GET',
+			// Everything that happens after clicking the random option 
+			success: function(data){
+				length = data.drinks.length;
+				drinkList = data.drinks
+				//console.log(drinkList[1].strDrink);
+				console.log(length);
+			},
+			error: function(data){
+
+			}
+		});
+		//after getting the drink list print them on the result list
+		setTimeout(function(){
+			for (i=0; i < length; i++){
+				//console.log(drinkName);
+				var drinkName = drinkList[i].strDrink;
+				//created to colve special characters conflict
+				
+				$('#list').append("<div id=" + "drink" + ">" + drinkName + "</div>" 
+		);
+				// $('#' + drinkNudeString).on('click',function(){
+					
+				// })
+				
+			}
 		},700);
+			$(document).on('click','#drink',function(){
+				var drinkName = this.innerHTML;
+				console.log(drinkName);
+				var drinkNudeString = drinkName.replace(/[&\/\\#,+()$~%.'":*?<>{} -!]/g,'');
+				$.ajax({
+					url: "http://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName,
+					dataType: 'json',
+					type: 'GET',
+					success: function(data){
+						name = data.drinks[0].strDrink;
+						img = data.drinks[0].strDrinkThumb;
+						console.log(data);
+						data1 = data;
+						instructions = data.drinks[0].strInstructions;
+						console.log(instructions);
+
+					}
+				})
+			setTimeout(function(){
+				$("#drinkName").hide();
+				$("#drinkName").attr('font-size', "100px;");
+				setValue('#drinkName', name);
+				//$('img').attr('src',img);
+				attribute('img','src',img);
+				//css('img','width','50px;');
+				//$('img').attr("width","200px");
+				css('img','height','100px;');
+				attribute('img','style','height:300px;')
+				setValue('#ingredients',ingredients)
+				while(str != ""){
+					str = data1.drinks[0]["strIngredient" + number];
+					amount = data1.drinks[0]["strMeasure" + number];
+					number = number + 1;
+					console.log(amount);
+					$('#ingredients').append("<p>" + amount + " " + str + "</p>");
+				}
+				$('#instructions').append("<p>"  + instructions + "</p");
+				//$('img').attr()
+			},700);
+		});
 		
 	});
 	
 
+
+	//Shopping cart remove from cart button
 	$(document).on('click','#removeItem',function(){
 		$('#singleItem').remove();
 	});
+
+	//Shopping cart Add to cart button
 	$('#addToCartButton').on('click', function(){
 		var value = document.getElementById('manualAdd').value;
 		if (value != ""){
 			$('#allItems').append("<div id=singleItem><div class= item id=cartItem>" + value +  "</div><div class= item id=removeItem><i class='fa fa-times fa-1x' aria-hidden=true></i></div></div>");
 		}
 	});
+
+	//Start Twilio stuff
 	const accountSid = "AC2fd72613e1ec4caa123859332ef62cfd";
 	const authToken = "c735425985c92c2694e5fe3b08e9b849";
 	var counter = 0;
@@ -133,6 +220,9 @@ $(document).ready(function() {
         });
 
 	});
+	var loadTemplate = function(){
+		$(".templateContainer").append('<object type="text/html" data="drinkTemplate1.html" ></object>');
+	}
 
 	
 });
