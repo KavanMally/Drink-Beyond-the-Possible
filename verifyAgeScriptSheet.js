8a88358f53a09dd2fa3b9c39f049c489c015aeff
David Navia <dan57@case.edu>
2017-11-28 19:58:03 -0500

    Timer works now
    Updates to verify Age so the timer works but isn't linked yet

--- verifyAgeScriptSheet.js
+++ verifyAgeScriptSheet.js
@@ -1,18 +1,17 @@
-// module.exports = {
-// function checkingAgeWrapper(){
-// 	input = document.getElementById('VABirthDay').value;
-// 	checkingAge(input);
-// }
+//module.exports = {
+	function checkingAgeWrapper(){
+ 	input = document.getElementById('VABirthDay').value;
+ 	checkingAge(input);
+ }
 
 
-//function checkingAge(input){
-exports.checkingAge = function(input){
+function checkingAge(input){
+//exports.checkingAge = function(input){
 
 	var userEnteredBirthDay = input;
-
 	// check to make sure something was entered
 	if (userEnteredBirthDay == ""){
-		//alert("Please enter your birthdate before entering.");
+		alert("Please enter your birthdate before entering.");
 		return;
 	}
 
@@ -28,35 +27,88 @@ exports.checkingAge = function(input){
 
 	// at some point edit this so it has fewer retern statments, atm it's neccesary.
 	if(bDayYear < minYear){
-		//alert("welcome");
-		return true;
+		alert("welcome");
+		//return true;
 	} else{
 		if (bDayYear == minYear){
 			if(bDayMonth < minMonth){
-				//alert("welcome");
-				return true;
+				alert("welcome");
+				//return true;
 			} else if(bDayMonth == minMonth){
 				if(bDayDay < minDay){
-					//alert("welcome");
-					return true;
+					alert("welcome");
+					//return true;
 				}
 			}
 		}	
 		// removes the input and locks the user out
 		//document.body.innerHTML = "You're too young to use this website!";
-		//document.body.style.fontSize = "75px";
-		//document.body.style.textAlign = "center";
+		document.body.style.fontSize = "75px";
+		document.body.style.textAlign = "center";
+		lockOutOfApp(bDayYear, bDayMonth, bDayDay, currentDate);
 		return false;
 	}
 
 }
 
+function lockOutOfApp(bDayYear, bDayMonth, bDayDay, currentDate){
+	document.getElementById("InputOutput").innerHTML = "You're too young to use this website!";
 
-/*function loadDoc() {
+
+	var daysLeft = 0;
+
+	// convirting years difference to days
+	bDayYear = bDayYear + 21; // goes from DOB to 21st birthday
+	bDayYear = bDayYear - currentDate.getFullYear(); // number of years till 21st birthday
+	daysLeft = (bDayYear * 365);
+
+	// convirting month difference to days 
+	var months = [0,31,59,90,120,151,181,212,243,273,304,334];
+
+	var BdayMToDays = months[bDayMonth - 1] + bDayDay;
+	var CDateMToDays = months[currentDate.getMonth()] + currentDate.getDate();
+
+	alert(BdayMToDays); //330
+	alert(CDateMToDays); // 328
+
+	if (BdayMToDays < CDateMToDays) 
+	{
+		daysLeft = daysLeft - CDateMToDays + BdayMToDays; 
+	}
+
+	if (BdayMToDays > CDateMToDays) {
+		daysLeft = daysLeft + BdayMToDays - CDateMToDays;
+	}
+
+
+	var x = setInterval(function(){daysLeft = countdown(daysLeft)}, 1000);
+}
+
+function countdown(daysLeft){
+	var currentDateAndTime = new Date();
+	var currentTime = currentDateAndTime.getTime();
+	var currentSecond = Math.floor(60 - currentTime % (1000 * 60) / 1000);
+	var currentMin = Math.floor(60 - currentTime % (1000 * 60 * 60) / (1000 * 60));
+	var currentHour = Math.floor(24 - currentTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+
+	// syncs hours up to complete when the timer hits 0
+	currentHour = (currentHour + 5) % 24;
+	
+
+	if (currentHour == 23 && currentMin == 59 && currentSecond == 59) 
+	{
+		daysLeft--;
+	}
+
+	document.getElementById("Countdown").innerHTML = " D-" + daysLeft + " H-" + currentHour + " Min-" + currentMin + " S-" + currentSecond;
+	return daysLeft;
+}
+
+function loadDoc() {
 	
   var xhttp = new XMLHttpRequest();
 
-	/*xmlhttp.onreadystatechange = function() {
+	xmlhttp.onreadystatechange = function() {
 			
 		if (xmlhttp.readyState == 1) {
 			document.body.innerHTML = "loading...";
@@ -65,11 +117,8 @@ exports.checkingAge = function(input){
 			//logToAnalytics(url);
 		}
 	};
-
-
 	
   xhttp.open("GET", "checklist.html", true);
   xhttp.send();
   alert("AHHHHHHHHHH");
 }
-*/
